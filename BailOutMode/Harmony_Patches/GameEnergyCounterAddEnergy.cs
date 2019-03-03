87bf7483e80e4e4c5fd497ca56013bff047cfcab
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-03-02 20:42:39 -0600

    More cleanup

--- BailOutMode/Harmony_Patches/GameEnergyCounterAddEnergy.cs
+++ BailOutMode/Harmony_Patches/GameEnergyCounterAddEnergy.cs
@@ -11,19 +11,6 @@ using static BailOutMode.ReflectionUtil;
 
 namespace BailOutMode.Harmony_Patches
 {
-    /* Not needed anymore
-    [HarmonyPatch(typeof(StandardLevelGameplayManager), "HandleGameEnergyDidReach0",
-        new Type[] { })]
-    class GameplayManagerStart
-    {
-        static bool Prefix()
-        {
-            Logger.Error("Uncaught fail detected");
-            return false;
-        }
-    }
-    */
-
     [HarmonyPatch(typeof(GameEnergyCounter), "AddEnergy",
         new Type[] {
         typeof(float)})]
@@ -39,7 +26,7 @@ namespace BailOutMode.Harmony_Patches
                     Logger.Debug("Fail detected. Current Energy: {0}, Energy Change: {1}", __instance.energy, value);
                     BS_Utils.Gameplay.ScoreSubmission.DisableSubmission(Plugin.PluginName);
                     Plugin._numFails++;
-                    //Console.WriteLine($"{__instance.energy} + {value} puts us <= 0");
+                    Logger.Debug($"{__instance.energy} + {value} puts us <= 0");
                     value = (Plugin.EnergyResetAmount / 100f) - __instance.energy;
                     Logger.Debug("Changing value to {0} to raise energy to {1}", value, Plugin.EnergyResetAmount);
                     if (Plugin.ShowFailText)
--- BailOutMode/LevelFailedEffectController.cs
+++ BailOutMode/LevelFailedEffectController.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using BS_Utils;
-using static BailOutMode.ReflectionUtil;
 
 namespace BailOutMode
 {
