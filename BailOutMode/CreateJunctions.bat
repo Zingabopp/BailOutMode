9d1104178db4378d83bfd28dda2b528e0c60e03d
Zingabopp <Zingabopp@gmail.com>
2020-03-14 19:21:01 -0500

    References fixed, version bump

--- BailOutMode/CreateJunctions.bat
+++ BailOutMode/CreateJunctions.bat
@@ -3,7 +3,7 @@ set SourcePath=%~1
 set DestPath=%~dp0
 
 if "%SourcePath%" == "" (
-	echo Enter the full path (without quotes^) to your Beat Saber game folder 
+	echo Enter the full path (without quotes^) to your Beat Saber game folder. Alternatively, you can drag and drop your Beat Saber folder onto this batch file to automatically make the junctions. 
 	set /p SourcePath="Path:"
 )
 
@@ -11,6 +11,8 @@ echo Source target: %SourcePath%
 echo Link target: %DestPath%
 set PluginPath=%SourcePath%\Plugins
 set ManagedPath=%SourcePath%\Beat Saber_Data\Managed
+set LibsPath=%SourcePath%\Libs
+set IPAPath=%SourcePath%\IPA
 
 if exist "%PluginPath%" (
 	echo Plugin folder exists, creating link
@@ -22,10 +24,24 @@ if exist "%PluginPath%" (
 if exist "%ManagedPath%" (
 	echo Managed folder exists, creating link
 	if not exist "%DestPath%References" mkdir "%DestPath%References"
-	mklink /J "%DestPath%References\Managed" "%ManagedPath%"
+	if not exist "%DestPath%References\Beat Saber_Data" mkdir "%DestPath%References\Beat Saber_Data"
+	mklink /J "%DestPath%References\Beat Saber_Data\Managed" "%ManagedPath%"
 ) else (
 	echo Managed folder missing
 )
-
+if exist "%LibsPath%" (
+	echo Libs folder exists, creating link
+	if not exist "%DestPath%References" mkdir "%DestPath%References"
+	mklink /J "%DestPath%References\Libs" "%LibsPath%"
+) else (
+	echo Libs folder missing
+)
+if exist "%IPAPath%" (
+	echo Libs folder exists, creating link
+	if not exist "%DestPath%References" mkdir "%DestPath%References"
+	mklink /J "%DestPath%References\IPA" "%IPAPath%"
+) else (
+	echo Libs folder missing
+)
 :End
 pause
